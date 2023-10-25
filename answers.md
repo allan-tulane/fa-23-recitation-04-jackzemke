cf02fd8d71463b3dfc021be3ac4e837a7845e2f8
Jack Zemke <95448108+jackzemke@users.noreply.github.com>
2023-10-25 14:30:03 -0500

    done?

--- answers.md
+++ answers.md
@@ -1,7 +1,7 @@
 # CMPS 2200 Reciation 5
 ## Answers
 
-**Name:**_________________________
+**Name:** Jack Zemke
 
 
 Place all written answers from `recitation-05.md` here for easier grading.
@@ -12,9 +12,87 @@ Place all written answers from `recitation-05.md` here for easier grading.
 
 
 
-- **1b.**
+ **1b.**
+  -
+
+I utilized the non-recursive version of `ssort` for this comparison. For my fixed pivot, I placed it at `list[len(list)//2]`, or the center of the list. This may explain it's acceptable performance on presorted lists as opposed to selecting `list[0]`. My system was exceeding maximum recursion depth with `list[0]` as the fixed pivot.
+
+Random Permutations:
+|     n (shuffled) |     ssort |   qsort-fixed-pivot |   qsort-random-pivot |
+|-------|-----------|---------------------|----------------------|
+|   100 |    0.105 |               0.075 |                0.090 |
+|   200 |    0.367 |               0.149 |                0.190 |
+|   500 |    2.118 |               0.366 |                0.541 |
+|  1000 |    8.196 |               0.800 |                1.162 |
+|  2000 |   32.604 |               1.722 |                2.535 |
+|  5000 |  218.558 |               5.168 |                7.078 |
+| 10000 |  887.802 |              10.826 |               14.042 |
+| 15000 | 2016.582 |              15.913 |               22.872 |
+| 20000 | 3600.353 |              22.465 |               31.529 |
+| 25000 | 5713.169 |              29.254 |               39.632 |
 
 
+![image](Figure_1.png "Fig 1")
+
+Unshuffled Permutations:
+|     n (presorted) |    ssort |   qsort-fixed-pivot |   qsort-random-pivot |
+|-------|----------|---------------------|----------------------|
+|   100 |    0.088 |               0.074 |                0.094 |
+|   200 |    0.303 |               0.149 |                0.199 |
+|   500 |    1.758 |               0.367 |                0.546 |
+|  1000 |    6.831 |               0.793 |                1.194 |
+|  2000 |   26.956 |               1.706 |                2.606 |
+|  5000 |  176.217 |               5.008 |                7.346 |
+| 10000 |  724.513 |              10.697 |               15.510 |
+| 15000 | 1641.346 |              16.011 |               23.143 |
+| 20000 | 2900.501 |              23.355 |               31.752 |
+| 25000 | 4465.927 |              29.481 |               39.743 |
+
+![image](Figure_2.png "Fig 2")
+
+The asymptotic bounds of these sorting methods are as follows:
+
+`ssort`: $\text{Average case time} = O(n^2)$ $\text{Worst case time} = O(n^2)$ 
+
+`qsort-fixed-pivot`: $\text{Average case time} = O(n \log n)$ $\text{Worst case time} = O(n^2)$
+
+`qsort-random-pivot`: $\text{Average case time} = O(n \log n)$ $\text{Worst case time} = O(n^2)$
+
+The running times line up with the asymptotic bounds. On average, both qsort algorithms solve faster than the ssort algorithm. Both of the qsort algorithms peform approximately the same on presorted and sorted lists, but ssort performs drastically better on presorted lists. 
 
 
-- **1c.**
+ **1c.**
+  -
+
+Shuffled:
+|     n (shuffled)  |   timsort |   qsort-fixed-pivot |
+|-------|-----------|---------------------|
+|   100 |     0.005 |               0.089 |
+|   200 |     0.010 |               0.177 |
+|   500 |     0.028 |               0.501 |
+|  1000 |     0.061 |               1.120 |
+|  2000 |     0.131 |               2.485 |
+|  5000 |     0.380 |               7.315 |
+| 10000 |     0.807 |              14.126 |
+| 15000 |     1.295 |              22.328 |
+| 20000 |     1.761 |              32.190 |
+| 25000 |     2.288 |              39.289 |
+
+![image](Figure_3.png "Fig 3")
+
+Unshuffled:
+|     n (presorted) |   timsort |   qsort-fixed-pivot |
+|-------|-----------|---------------------|
+|   100 |     0.001 |               0.075 |
+|   200 |     0.001 |               0.148 |
+|   500 |     0.002 |               0.398 |
+|  1000 |     0.004 |               0.850 |
+|  2000 |     0.008 |               1.929 |
+|  5000 |     0.022 |               4.977 |
+| 10000 |     0.041 |              10.578 |
+| 15000 |     0.057 |              15.906 |
+| 20000 |     0.100 |              22.514 |
+| 25000 |     0.109 |              29.274 |
+
+![image](Figure_4.png "Fig 4")
+
--- main.py
+++ main.py
@@ -3,8 +3,32 @@ import tabulate
 
 
 def qsort(a, pivot_fn):
-    ## TO DO
-    pass
+    # print(a)
+    if len(a) == 0 or len(a) == 1:
+        return a
+    p = pivot_fn(a)
+    # print(p)
+    l = list(filter(lambda x: x<p, a))
+    r = list(filter(lambda x: x>p, a))
+    sl = qsort(l,pivot_fn)
+    sr = qsort(r,pivot_fn)
+    return sl + [p] + sr
+
+# print(qsort((list(range(100))), random.choice))
+# print(qsort([3,8,7,1,4,9,10,42], lambda x: x[0]))
+# a = (list(range(1000)))
+# random.shuffle(a)
+# print(a)
+# print(qsort(a,random.choice))
+# print(random.shuffle(list(range(1,100))))
+
+def ssort(L):
+    for i in range(len(L)):
+        # print(L)
+        m = L.index(min(L[i:]))
+        L[i], L[m] = L[m], L[i]
+    return L
+
     
 def time_search(sort_fn, mylist):
     """
@@ -29,7 +53,7 @@ def time_search(sort_fn, mylist):
     return (time.time() - start) * 1000
     ###
 
-def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 100000]):
+def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 15000, 20000, 25000]):
     """
     Compare the running time of different sorting algorithms.
 
@@ -40,32 +64,110 @@ def compare_sort(sizes=[100, 200, 500, 1000, 2000, 5000, 10000, 20000, 50000, 10
       for each method to run on each value of n
     """
     ### TODO - sorting algorithms for comparison
-    qsort_fixed_pivot = # 
-    qsort_random_pivot = #
-    tim_sort = #
+    def qsort_fixed_pivot(a):
+        return qsort(a, lambda x: x[len(x)//2])
+    def qsort_random_pivot(a):
+        return qsort(a, random.choice)
+    tim_sort = sorted
     result = []
     for size in sizes:
         # create list in ascending order
         mylist = list(range(size))
         # shuffles list if needed
-        #random.shuffle(mylist)
+        # random.shuffle(mylist)
+        # print(mylist)
+        # print(mylist)
         result.append([
             len(mylist),
+            time_search(tim_sort, mylist),
             time_search(qsort_fixed_pivot, mylist),
-            time_search(qsort_random_pivot, mylist),
+            # time_search(qsort_random_pivot, mylist),
         ])
     return result
     ###
 
 def print_results(results):
     """ change as needed for comparisons """
-    print(tabulate.tabulate(results,
-                            headers=['n', 'qsort-fixed-pivot', 'qsort-random-pivot'],
+    a = tabulate.tabulate(results,
+                            headers=['n', 'timsort', 'qsort-fixed-pivot', 'qsort-random-pivot'],
                             floatfmt=".3f",
-                            tablefmt="github"))
+                            tablefmt="github")
+    print(a)
+    return a
 
+results = compare_sort()
 def test_print():
     print_results(compare_sort())
 
 random.seed()
 test_print()
+
+# # plot raw timing data and best fit curves.
+
+# import matplotlib.pyplot as plt
+# # %matplotlib inline
+# import math
+# from scipy.optimize import curve_fit
+# import numpy as np
+
+# sizes = [x[0] for x in results]
+# linear_times = [x[3] for x in results]
+# binary_times = [x[2] for x in results]
+# qsort_random = [x[3] for x in results]
+# fig, ax = plt.subplots(1)
+# ax2 = ax.twinx()
+# ax2.plot(
+#          sizes,
+#          linear_times,
+#          'bo-'
+#        )
+# ax2.set_ylabel('ssort', color='blue', size=20)
+# ax2.tick_params(axis='y', labelcolor='blue')
+# ax.plot(
+#          sizes,
+#          binary_times,
+#          'ro-'
+#         )
+# ax.set_ylabel('qsort-fixed', color='red', size=20)
+# ax.tick_params(axis='y', labelcolor='red')
+# ax.set_xlabel('n', size=20)
+
+# def lgn(i, beta, beta0):
+#     # beta*lg(n) + beta0
+#     return beta*np.array([math.log(x, 2) for x in i] ) + beta0
+
+# def linearn(i, beta, beta0):
+#     # beta*n + beta0
+#     return beta*np.array(i) + beta0
+
+# lgn_beta, _ = curve_fit(lgn, sizes, binary_times)
+# ax.plot(sizes, lgn(sizes, *lgn_beta), 'r--', label='lg fit (beta=%.2g, beta0=%.2g)' % (lgn_beta[0], lgn_beta[1]))
+# ax.legend(loc='upper left')
+
+# linearn_beta, _ = curve_fit(linearn, sizes, linear_times)
+# ax2.plot(sizes, linearn(sizes, *linearn_beta), 'b--', label='linear fit (beta=%.2g, beta0=%.2g)' % (linearn_beta[0], linearn_beta[1]))
+# ax2.legend(loc='lower right')
+# plt.show()
+
+import matplotlib.pyplot as plt
+
+def graph():
+    sizes = [x[0] for x in results]
+    ssort = [x[1] for x in results]
+    qsort_fixed = [x[2] for x in results]
+    # qsort_random = [x[3] for x in results]
+    # print(l)
+    # print(n)
+    # print(t)
+    plt.plot(sizes,ssort, label = 'timsort')
+    plt.plot(sizes,qsort_fixed, label = 'qsort_fixed')
+    # plt.plot(sizes,qsort_random, label = 'qsort_random')
+    plt.legend()
+    plt.title('timsort vs qsort_fixed')
+    plt.xlabel('size of input list, presorted')
+    plt.ylabel('Time required to execute')
+    plt.yscale('symlog')
+    plt.xscale('log')
+    plt.show()
+
+graph()
